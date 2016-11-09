80fd38b5dd5ba974c864cb23f59d9fa23defaa05
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-09 16:03:14 +0100

    add text input component

--- src/searchbox/componnets/textInput.tsx
+++ src/searchbox/componnets/textInput.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
+import {ENTER_KEY} from "../../global/constants";
 
 export interface ITextInputProps {
-    onSearch : (value: string) => void;
+    onSearch: (value: string) => void;
     text: string;
     placeholder: string;
     search: boolean;
@@ -11,8 +12,36 @@ export interface ITextInputState {
     text: string;
 }
 
-export class TextInput extends React.Component<ITextInputProps, ITextInputState>{
+export class TextInput extends React.Component<ITextInputProps, ITextInputState> {
+
+    constructor(props: ITextInputProps) {
+        super(props);
+        this.state = {text: ""};
+    }
+
+    public handleSubmit(e: React.KeyboardEvent) {
+        if (e.which === ENTER_KEY) {
+            const text = (e.target as any).value as string;
+            this.props.onSearch(text);
+            if (this.props.search) {
+                this.setState({text: ""});
+            }
+        }
+    }
+
+    public handleChange(e: React.SyntheticEvent) {
+        this.setState({text: (e.target as any).value})
+    }
+
+    public handleBlur(e: React.SyntheticEvent) {
+        if (!this.props.search) {
+            this.props.onSearch((e.target as any).value);
+        }
+    }
+
     public render() {
-        return <h1></h1>;
+        return <input type="text" name="searchTweet" id="tweet-search" className="input-lg form-control"
+                      onBlur={this.handleBlur} onKeyDown={this.handleSubmit} onChange={this.handleChange}
+                      autoFocus={this.state.text.length > 0} placeholder={this.props.placeholder}/>;
     }
 }

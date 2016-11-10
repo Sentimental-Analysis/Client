a22b3fc92f5680c3c8c05baf62c0744a88377172
Dominik Kotecki <dominik_kotecki@rossmann.com.pl>
2016-11-10 15:12:14 +0100

    fix searchTweetsBy key type conflict error

--- src/global/reducers.tsx
+++ src/global/reducers.tsx
@@ -6,12 +6,12 @@ export type Reducer<TState> = (state: TState, action: Action) => TState;
 
 export type Reducers = { tweets: Reducer<Tweet[]>, isFetching: Reducer<boolean>, error: Reducer<string> };
 
-export function isFetching(state: boolean = false, action: Action){
+export function isFetching(state: boolean = false, action: Action) {
     return action.type === "SEARCH_PENDING";
 }
 
 export function error(state: string = "", action: Action) {
-    if(action.type === "SEARCH_REJECTED"){
+    if(action.type === "SEARCH_REJECTED") {
         return action.payload;
     }
     return state;
--- src/searchbox/searchBoxReducer.tsx
+++ src/searchbox/searchBoxReducer.tsx
@@ -3,7 +3,10 @@ import {Action} from "../global/actionTypes";
 
 export function searchBoxReducer(state: Tweet[] = [], action: Action) {
     if(action.type === "SEARCH_FULFILLED") {
-        return action!.payload;
+        if(action!.payload.isSuccess){
+            return action!.payload.value;
+        }
+        return state;
     }
     return state;
 }

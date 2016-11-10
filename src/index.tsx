ed361cfc711c4ca7a126afa3d1a45cb40724a83d
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-10 16:38:32 +0100

    small changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import * as React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
-import {Router, Route, browserHistory} from 'react-router'
-import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
 import {reducers} from "./global/reducers";
 import {App} from "./app/app";
 import * as logger from "redux-logger"
@@ -17,11 +15,9 @@ const initialState: AppState = {
     error: ""
 };
 
-const combinedReducers = combineReducers(Object.assign({}, reducers, {routing: routerReducer}));
+const combinedReducers = combineReducers(reducers);
 const middleware = applyMiddleware(promise(), thunk, logger());
 const store = createStore(combinedReducers, initialState, middleware);
-
-const history = syncHistoryWithStore(browserHistory, store);
 
 store.subscribe(() => {
     console.log("changed", store.getState());

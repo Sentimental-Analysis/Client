61ba1795aec252a0c7e3e5966804d83a343e7604
dominikus1993 <dominikus1910@gmail.com>
2016-11-11 10:51:27 +0100

    format

--- src/searchbox/componnets/textInput.tsx
+++ src/searchbox/componnets/textInput.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {ENTER_KEY} from "../../global/constants";
+import { ENTER_KEY } from "../../global/constants";
 
 export interface ITextInputProps {
     onSearch: (value: string) => void;
@@ -16,7 +16,7 @@ export class TextInput extends React.Component<ITextInputProps, ITextInputState>
 
     constructor(props: ITextInputProps) {
         super(props);
-        this.state = {text: ""};
+        this.state = { text: "" };
     }
 
     public handleSubmit(e: React.KeyboardEvent) {
@@ -24,13 +24,13 @@ export class TextInput extends React.Component<ITextInputProps, ITextInputState>
             const text = (e.target as any).value as string;
             this.props.onSearch(text);
             if (this.props.search) {
-                this.setState({text: ""});
+                this.setState({ text: "" });
             }
         }
     }
 
     public handleChange(e: React.SyntheticEvent) {
-        this.setState({text: (e.target as any).value})
+        this.setState({ text: (e.target as any).value })
     }
 
     public handleBlur(e: React.SyntheticEvent) {
@@ -41,7 +41,7 @@ export class TextInput extends React.Component<ITextInputProps, ITextInputState>
 
     public render() {
         return <input type="text" name="searchTweet" id="tweet-search" className="input-lg form-control"
-                      onBlur={this.handleBlur} onKeyDown={this.handleSubmit} onChange={this.handleChange}
-                      autoFocus={this.state.text.length > 0} placeholder={this.props.placeholder}/>;
+            onBlur={this.handleBlur} onKeyDown={this.handleSubmit} onChange={this.handleChange}
+            autoFocus={this.state.text.length > 0} placeholder={this.props.placeholder} />;
     }
 }

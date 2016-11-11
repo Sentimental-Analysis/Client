31e7d31a9c50d88f0b9a2aa6bbf31be868f60973
dominikus1993 <dominikus1910@gmail.com>
2016-11-11 16:23:25 +0100

    add Submit button to searchbox component

--- src/searchbox/searchBoxContainer.tsx
+++ src/searchbox/searchBoxContainer.tsx
@@ -1,3 +1,4 @@
+import { SubmitButton } from './componnets/submitButton';
 import { TextInput } from './componnets/textInput';
 import { Action } from '../global/actionTypes';
 import * as React from 'react';
@@ -10,7 +11,12 @@ export class SearchBox extends React.Component<ISearchBoxProps, any> {
     public render() {
         return <div className="searchBox row">
                     <div className="col-lg-12" >
-                        <TextInput text={""} placeholder="Wpisz fraze" search={true} onSearch={this.props.Search} ></TextInput>
+                        <div className="input-group input-group-lg" >
+                            <TextInput text={""} placeholder="Wpisz fraze" search={true} onSearch={this.props.Search} ></TextInput>
+                            <span className="input-group-btn" >
+                                <SubmitButton value="Szukaj" ></SubmitButton>
+                            </span>
+                        </div>                       
                     </div>
                </div>;
     }

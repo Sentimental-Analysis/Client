52a931a1b19f2e9e9f85e27694f77ff310c22454
dominikus1993 <dominikus1910@gmail.com>
2016-11-13 11:58:56 +0100

    add line chart

--- src/d3/lineChart.tsx
+++ src/d3/lineChart.tsx
@@ -1,6 +1,34 @@
 import * as React from "react";
 import * as d3 from "d3";
-import {Chart} from "./chart";
-import {Axis} from "./axis";
-import {Path} from "./path";
-import {ToolTip} from "./tooltip";
+import { Chart } from "./chart";
+import { Axis } from "./axis";
+import { Path } from "./path";
+import { ToolTip } from "./tooltip";
+import { DataSet } from "./dataSet";
+
+export interface ILineChartProps extends React.HTMLAttributes {
+
+}
+
+export class LineChart extends React.Component<ILineChartProps, any> {
+    public render() {
+        const {
+            height,
+            width,
+            margin,
+            colorScale,
+            interpolate,
+            defined,
+            stroke,
+            values,
+            label,
+            x,
+            y,
+            xAxis,
+            yAxis,
+            shape,
+            shapeColor
+        } = this.props;
+        return <h1></h1>
+    }
+}

ad17952c690e629ef1d9bdbb81a4c053c1804a7c
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-29 21:28:48 +0100

    add detail component

--- src/details/deatilContainer.tsx
+++ src/details/deatilContainer.tsx
@@ -1,3 +1,4 @@
+import { KeyWordList } from "./components/keywordList";
 import * as React from "react";
 import { Score } from "../global/model";
 import Opinion from "./components/opinion";
@@ -11,6 +12,7 @@ class Details extends React.Component<IDetailsProps, any> {
       <h1 className="text">{this.props.score.key}</h1>
       <div>
         <Opinion negativeOpinionQuantity={this.props.score.negativeTweetsQuantity} positiveOpinionQuantity={this.props.score.positiveTweetsQuantity} sentiment={this.props.score.sentiment}></Opinion>
+        <KeyWordList keywords={this.props.score.keyWords}></KeyWordList>
       </div>
     </div>);
   };

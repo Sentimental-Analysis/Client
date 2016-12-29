64c51f785010986e5e016be2f438b18f6bb3eda1
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-29 19:15:14 +0100

    add keywordList component

---
+++ src/details/components/keywordList.tsx
@@ -0,0 +1,23 @@
+import { link } from 'fs';
+import { render } from 'enzyme';
+import { KeyWord } from '../../global/model';
+import * as React from "react";
+import { List } from "immutable";
+
+export interface KeyWordListProps {
+    keywords: KeyWord[]
+}
+
+export class KeyWordList extends React.Component<KeyWordListProps, any> {
+    render() {
+        const tmp = List.of(...this.props.keywords).take(10);
+        return <ul className="list-group" >
+            {tmp.map(keyword => {
+                return <li key={`${keyword.key}-${keyword.quantity}`} className="list-group-item" >
+                    {keyword.key}
+                    <span className="badge" >{keyword.quantity}</span>
+                </li>
+            }).toArray()}
+        </ul>
+    }
+}

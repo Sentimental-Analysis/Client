57219abd439a291342cfe27b430553822e16b217
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-29 20:27:50 +0100

    remove d3

---
+++ src/details/components/opinion.tsx
@@ -0,0 +1,47 @@
+import { Sentiment } from '../../global/model';
+import * as React from "react";
+
+interface IOpinionProps {
+  negativeOpinionQuantity: number;
+  positiveOpinionQuantity: number;
+  sentiment: Sentiment;
+};
+
+interface IOpinionState { };
+
+function rateSentiment(sentiment: Sentiment): "Neutralny" | "Pozytywny" | "Negatywny" {
+    switch(sentiment){
+        case Sentiment.Neutral:
+            return "Neutralny";
+        case Sentiment.Negative:
+            return "Negatywny";
+        case Sentiment.Positive:
+            return "Pozytywny";
+        default:
+            return "Neutralny";
+    }
+}
+
+class Opinion extends React.Component<IOpinionProps, IOpinionState> {
+  public render(): JSX.Element {
+    return (<div className="score">
+      <div className="details">
+        <div className="row text">
+          <div className="col-md-4 well nopadding scoreBox">
+            <h1 id="positive-quantity">Ilość pozytywnych opini</h1>
+            <h1>{this.props.positiveOpinionQuantity}</h1>
+          </div>
+          <div className="col-md-4 well nopadding scoreBox">
+            {rateSentiment(this.props.sentiment)}
+          </div>
+          <div className="col-md-4 well nopadding scoreBox">
+            <h1>Ilość negatywnych opini</h1>
+            <h1 id="negative-quantity" >{this.props.negativeOpinionQuantity}</h1>
+          </div>
+        </div>
+      </div>
+    </div>)
+  }
+}
+
+export default Opinion;

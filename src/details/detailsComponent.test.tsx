f588e5dd0f4fa66c8f4c5295afe199c0e5e278d4
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-30 17:31:19 +0100

    add describe

--- src/details/detailsComponent.test.tsx
+++ src/details/detailsComponent.test.tsx
@@ -5,12 +5,15 @@ import {Action} from "../global/actionTypes";
 import {Sentiment} from "../global/model";
 import {sentimentReducer} from "./detailReducers";
 
-describe("detailsReducers", () => {
-   describe("sentimentReducer", () => {
-      it("action is fulfilled", () => {
-          const action: Action = {type: "SEARCH_FULFILLED", payload: { status: 200, data: { isSuccess: true, value: { sentiment: Sentiment.Positive} } } as any };
-          const result = sentimentReducer("Neutralny", action);
-          expect(result).eq("Pozytywny");
-      });
-   });
+describe("<Details>", () => {
+    describe("detailsReducers", () => {
+        describe("sentimentReducer", () => {
+            it("action is fulfilled", () => {
+                const action: Action = {type: "SEARCH_FULFILLED", payload: { status: 200, data: { isSuccess: true, value: { sentiment: Sentiment.Positive} } } as any };
+                const result = sentimentReducer("Neutralny", action);
+                expect(result).eq("Pozytywny");
+            });
+        });
+    });
 });
+
